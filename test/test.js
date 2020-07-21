5942fae453b5911ef7683482e78ea3b7d04fa383
Haley <h.rohe96@gmail.com>
2020-07-21 14:07:25 -0400

    created triangle.js and test.js and added code

---
+++ test/test.js
@@ -0,0 +1,7 @@
+var area = reqire('../triangle.js');
+
+describe('Area', function (){
+    it('should return 10 when first is 2 and second is 5', function(){
+        assert.equal(area(2,5), 10);
+    });
+});
