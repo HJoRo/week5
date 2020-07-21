10bb1eabb82bc4746936e67c9f7ad70d2cffcbc4
Haley <h.rohe96@gmail.com>
2020-07-21 18:47:20 -0400

    added comments

--- objects.js
+++ objects.js
@@ -1,10 +1,12 @@
 var harryPotter = {
+    //properties
     title: "Harry Potter and the Order of the Phoenix",
     author: "J. K. Rowling",
     genre: "Fantasy",
     description: "In his 5th year at Hogwarts, Harry and his friends in the Order of the Phoenix must fight Voldemort for control of an important secret weapon.",
     price: "$13.50",
     seller: "Amazon.com",
+    //modifiers
     buyNow : function (){
         return this.price + " on " + this.seller;
     },
@@ -12,5 +14,6 @@ var harryPotter = {
      return this.title + ", " + this.genre + ". " + this.description;
    }
 }
+//call functions
 console.log(harryPotter.buyNow());
 console.log(harryPotter.learnMore());
